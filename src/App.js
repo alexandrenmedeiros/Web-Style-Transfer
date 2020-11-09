cd723491d24a032f777ce28dbd222dbf37dcf818
Alexandre Medeiros <xandynorcia@gmail.com>
2020-11-09 11:30:41 -0300

    basic state change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
+
+  // step says wich step the user is on, 
+  // 0: loading model, 1: input step, 2: output step
+  const [step, setStep] = React.useState(0)
+
+  const loadModel = async () => {
+    setTimeout(() => {
+      setStep(1)
+    }, 3000);
+  }
+
+  const styleImage = async () => {
+    setStep(2)
+  }
+
+  const reset = async () => {
+    setStep(1)
+  }
+
+  loadModel()
+  const buttonFunction = {
+    0 : {text: 'Loading Model ...', func: () => {}},
+    1 : {text: 'Style Image', func: styleImage},
+    2 : {text: 'New Style Transfer', func: reset}
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={buttonFunction[step].func}>{buttonFunction[step].text}</button>
     </div>
   );
 }
