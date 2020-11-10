52dbd3d35d9af9f3d25babf93125be349ad617cc
Alexandre Medeiros <xandynorcia@gmail.com>
2020-11-10 14:26:10 -0300

    loading model from magenta

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import React from 'react'
+import * as mi from '@magenta/image'
 
 function App() {
 
   // step says wich step the user is on, 
   // 0: loading model, 1: input step, 2: output step
   const [step, setStep] = React.useState(0)
+  const [model, setModel] = React.useState(null)
 
   const loadModel = async () => {
-    setTimeout(() => {
-      setStep(1)
-    }, 3000);
+    // setTimeout(() => {
+    //   setStep(1)
+    // }, 3000);
+
+    const model =  mi.ArbitraryStyleTransferNetwork();
+    setModel(model)
+
+    setStep(1)
   }
 
   const styleImage = async () => {
@@ -21,9 +28,8 @@ function App() {
     setStep(1)
   }
 
-  loadModel()
   const buttonFunction = {
-    0 : {text: 'Loading Model ...', func: () => {}},
+    0 : {text: 'Load Model', func: loadModel},
     1 : {text: 'Style Image', func: styleImage},
     2 : {text: 'New Style Transfer', func: reset}
   }
