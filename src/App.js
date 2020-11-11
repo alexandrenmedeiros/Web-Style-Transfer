b127a894a31db034cf20445d6e1106bda653bcb4
Alexandre Medeiros <xandynorcia@gmail.com>
2020-11-11 14:52:02 -0300

    simple content image upload

--- src/App.js
+++ src/App.js
@@ -8,8 +8,24 @@ function App() {
   // 0: loading model, 1: input step, 2: output step
   const [step, setStep] = React.useState(0)
   const [model, setModel] = React.useState(null)
+  const [contentImageUrl, setContentImageUrl] = React.useState(null)
+  const [showContentImage, setShowContentImage] = React.useState(false)
 
+  // aux functions
+  const handleUpload = Event => {
+    const img = Event.target.files[0]
+
+    if (img != null) {
+      const url = URL.createObjectURL(img)
+      setContentImageUrl(url)
+      setShowContentImage(true)
+    }
+  }
+
+  // State machine on site functions
   const loadModel = async () => {
+    // (step 0) first state, load model in the browser
+
     // setTimeout(() => {
     //   setStep(1)
     // }, 3000);
@@ -21,13 +37,18 @@ function App() {
   }
 
   const styleImage = async () => {
+    // (step 1) second state, page to set content and style images
+
     setStep(2)
+    setShowContentImage(false) // dont show image preview after button press
   }
 
   const reset = async () => {
+    // (step 2) third state, show model output and can go back to second state
     setStep(1)
   }
 
+  // button actions to swap through states
   const buttonFunction = {
     0 : {text: 'Load Model', func: loadModel},
     1 : {text: 'Style Image', func: styleImage},
@@ -36,6 +57,9 @@ function App() {
 
   return (
     <div className="App">
+      {step === 1 && <input type='file' accept='image/*' onChange={handleUpload}/>}
+      {showContentImage === true && <img src={contentImageUrl} alt="upload-preview" />}
+
       <button onClick={buttonFunction[step].func}>{buttonFunction[step].text}</button>
     </div>
   );
