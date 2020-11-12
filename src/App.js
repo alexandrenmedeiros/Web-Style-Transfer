451906c591110fce61258a40628f8330b4b15f65
Alexandre Medeiros <xandynorcia@gmail.com>
2020-11-12 16:59:22 -0300

    resize content image range

--- src/App.js
+++ src/App.js
@@ -46,9 +46,11 @@ function App() {
   const [model, setModel] = React.useState(null)
 
   // Content image variables
+  const [contentImage, setContentImage] = React.useState(null)
   const [contentImageUrl, setContentImageUrl] = React.useState(null)
   const [showContentImage, setShowContentImage] = React.useState(false)
   const [contentImageCanvas, setContentImageCanvas] = React.useState(null)
+  const [maxSizeContentImage, setMaxSizeContentImage] = React.useState(400)
 
   // style image variables
   const [styleImageUrl, setStyleImageUrl] = React.useState(null)
@@ -60,30 +62,43 @@ function App() {
   const [showOutputImage, setShowOutputImage] = React.useState(false)
 
   const styleOptions = [
-    {label: 'kanagawa_great_wave', value: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg'},
-    {label: 'kandinsky_composition_7', value: 'https://upload.wikimedia.org/wikipedia/commons/b/b4/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg'},
-    {label: 'van_gogh_starry_night', value: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'},
-    {label: 'turner_nantes', value: 'https://upload.wikimedia.org/wikipedia/commons/b/b7/JMW_Turner_-_Nantes_from_the_Ile_Feydeau.jpg'},
-    {label: 'munch_scream', value: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Edvard_Munch%2C_1893%2C_The_Scream%2C_oil%2C_tempera_and_pastel_on_cardboard%2C_91_x_73_cm%2C_National_Gallery_of_Norway.jpg'},
-    {label: 'picasso_demoiselles_avignon', value: 'https://upload.wikimedia.org/wikipedia/en/4/4c/Les_Demoiselles_d%27Avignon.jpg'},
-    {label: 'picasso_violin', value: 'https://upload.wikimedia.org/wikipedia/en/3/3c/Pablo_Picasso%2C_1911-12%2C_Violon_%28Violin%29%2C_oil_on_canvas%2C_Kr%C3%B6ller-M%C3%BCller_Museum%2C_Otterlo%2C_Netherlands.jpg'},
-    {label: 'picasso_bottle_of_rum', value: 'https://upload.wikimedia.org/wikipedia/en/7/7f/Pablo_Picasso%2C_1911%2C_Still_Life_with_a_Bottle_of_Rum%2C_oil_on_canvas%2C_61.3_x_50.5_cm%2C_Metropolitan_Museum_of_Art%2C_New_York.jpg'},
-    {label: 'fire', value: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Large_bonfire.jpg'},
-    {label: 'derkovits_woman_head', value: 'https://upload.wikimedia.org/wikipedia/commons/0/0d/Derkovits_Gyula_Woman_head_1922.jpg'},
-    {label: 'amadeo_style_life', value: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Untitled_%28Still_life%29_%281913%29_-_Amadeo_Souza-Cardoso_%281887-1918%29_%2817385824283%29.jpg'},
-    {label: 'derkovtis_talig', value: 'https://upload.wikimedia.org/wikipedia/commons/3/37/Derkovits_Gyula_Talig%C3%A1s_1920.jpg'},
-    {label: 'amadeo_cardoso', value: 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Amadeo_de_Souza-Cardoso%2C_1915_-_Landscape_with_black_figure.jpg'}
+    {label: 'Kanagawa Great Wave', value: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg'},
+    {label: 'Kandinsky Composition 7', value: 'https://upload.wikimedia.org/wikipedia/commons/b/b4/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg'},
+    {label: 'Van Gogh Starry Night', value: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'},
+    {label: 'Turner Nantes', value: 'https://upload.wikimedia.org/wikipedia/commons/b/b7/JMW_Turner_-_Nantes_from_the_Ile_Feydeau.jpg'},
+    {label: 'Munch Scream', value: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Edvard_Munch%2C_1893%2C_The_Scream%2C_oil%2C_tempera_and_pastel_on_cardboard%2C_91_x_73_cm%2C_National_Gallery_of_Norway.jpg'},
+    {label: 'Picasso Demoiselles Avignon', value: 'https://upload.wikimedia.org/wikipedia/en/4/4c/Les_Demoiselles_d%27Avignon.jpg'},
+    {label: 'Picasso Violin', value: 'https://upload.wikimedia.org/wikipedia/en/3/3c/Pablo_Picasso%2C_1911-12%2C_Violon_%28Violin%29%2C_oil_on_canvas%2C_Kr%C3%B6ller-M%C3%BCller_Museum%2C_Otterlo%2C_Netherlands.jpg'},
+    {label: 'Picasso Bottle of Rum', value: 'https://upload.wikimedia.org/wikipedia/en/7/7f/Pablo_Picasso%2C_1911%2C_Still_Life_with_a_Bottle_of_Rum%2C_oil_on_canvas%2C_61.3_x_50.5_cm%2C_Metropolitan_Museum_of_Art%2C_New_York.jpg'},
+    {label: 'Fire', value: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Large_bonfire.jpg'},
+    {label: 'Derkovits Woman Head', value: 'https://upload.wikimedia.org/wikipedia/commons/0/0d/Derkovits_Gyula_Woman_head_1922.jpg'},
+    {label: 'Amadeo Style Life', value: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Untitled_%28Still_life%29_%281913%29_-_Amadeo_Souza-Cardoso_%281887-1918%29_%2817385824283%29.jpg'},
+    {label: 'Derkovtis Talig', value: 'https://upload.wikimedia.org/wikipedia/commons/3/37/Derkovits_Gyula_Talig%C3%A1s_1920.jpg'},
+    {label: 'Amadeo Cardoso', value: 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Amadeo_de_Souza-Cardoso%2C_1915_-_Landscape_with_black_figure.jpg'},
+    {label: 'Hand with Reflecting Sphere', value: 'https://upload.wikimedia.org/wikipedia/en/6/66/Hand_with_Reflecting_Sphere.jpg'},
+    { label: 'Vitral', value: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/England_YorkMinster_JesseTree_c1170.JPG'}
   ]
 
   // handle functions
   const handleUpload = Event => {
     const img = Event.target.files[0]
+    setContentImage(img)
 
     if (img != null) {
-
-      resizeImageUrl(img, setContentImageUrl, setContentImageCanvas, 600)
+      resizeImageUrl(img, setContentImageUrl, setContentImageCanvas, maxSizeContentImage)
       setShowContentImage(true)
     }
+  }
+
+  const handleMaxSizeContent = Event => {
+    setTimeout(() => {
+      console.log(Event.target.value)
+      setMaxSizeContentImage(Event.target.value)
+
+      if (contentImage != null) {
+        resizeImageUrl(contentImage, setContentImageUrl, setContentImageCanvas, Event.target.value)
+      }
+    }, 60)
   }
 
   const handleStyleSelect = selectedStyleOption => {
@@ -92,7 +107,7 @@ function App() {
       fetch(selectedStyleOption.value)
         .then(response => response.blob())
         .then(img =>{
-          resizeImageUrl(img, setStyleImageUrl, setStyleImageCanvas, 256)
+          resizeImageUrl(img, setStyleImageUrl, setStyleImageCanvas, 250)
           setShowStyleImage(true)
         })
     }
@@ -136,6 +151,7 @@ function App() {
 
     setContentImageUrl(null)
     setStyleImageUrl(null)
+    setOutputImageUrl(null)
     setShowOutputImage(false)
     setStep(1)
   }
@@ -151,6 +167,7 @@ function App() {
   return (
     <div className="App">
       {step === 1 && <input type='file' accept='image/*' onChange={handleUpload}/>}
+      {step === 1 && <input type='range' min='100' max='800' defaultValue='400' onChange={handleMaxSizeContent} />}
       {showContentImage === true && <img src={contentImageUrl} alt="upload-preview" />}
 
       {step === 1 && <Select options={styleOptions} onChange={handleStyleSelect} />}
